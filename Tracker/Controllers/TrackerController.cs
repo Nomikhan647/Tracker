bdaea01cf355004896004cb82ade6ed8cd561052
Nauman khan <muhammmadnauman992001647@gmail.com>
2024-10-17 10:10:36 +0500

    Authorization added

--- Tracker/Controllers/TrackerController.cs
+++ Tracker/Controllers/TrackerController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Tracker.Controllers
 {
+    [Authorize]
     public class TrackerController : Controller
     {
         public IActionResult Index()
